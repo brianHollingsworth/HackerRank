303e2b84a83064ef11bdc62cb8bf0c8df84a0424
Brian Hollingsworth <brianHollingsworth@users.noreply.github.com>
2016-10-14 16:27:20 -0500

    Delete Correlation_And_Regression_Lines_1.py

--- Artificial_Intelligence/Statistics_And_Machine_Learning/Correlation_And_Regression_Lines_1.py
+++
@@ -1,9 +0,0 @@
-# Here are the test scores of 10 students in physics and history:
-# Physics Scores  15  12  8   8   7   7   7   6   5   3
-# History Scores  10  25  17  11  13  17  20  13  9   15
-# Compute Karl Pearson’s coefficient of correlation between these scores.
-#
-# Output Format
-#
-# In the text box, enter the floating point/decimal value required. Do not leave
-# any leading or trailing spaces. Your answer may look like: 0.255
