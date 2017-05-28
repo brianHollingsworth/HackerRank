64062c719dd0e671cc02b718302835dadfe75dde
Brian Hollingsworth <bhollin42@gmail.com>
2017-05-28 15:33:21 -0500

    SQL Challenges

--- SQL/Alternative_Queries/DrawTheTriangle2.sql
+++ SQL/Alternative_Queries/DrawTheTriangle2.sql
@@ -8,5 +8,5 @@
 -- * * * * *
 -- Write a query to print the pattern P(20).
 
-SELECT rpad('* ',LEVEL*2,'* ')
-FROM dual CONNECT BY LEVEL <=20;
+SELECT RPAD('*',LEVEL*2,' *')
+FROM DUAL CONNECT BY LEVEL <= 20;
